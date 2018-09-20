4e44ffccefdb7116de586a0a7d81ad293772fa02
optimasqaud <33160067+optimasquad@users.noreply.github.com>
2018-09-20 11:49:51 -0700

    Update Testing.java

--- dateUtils/src/main/java/com/mkyong/core/utils/Testing.java
+++ dateUtils/src/main/java/com/mkyong/core/utils/Testing.java
@@ -14,6 +14,8 @@ import java.util.Map;
  */
 public class Testing {
 
+	private static boolean updateColTagFlag = false;
+
 	public static void main(String args[]) {
 
 		List<PcolDef> pcolDefList = new ArrayList<>();
@@ -26,17 +28,36 @@ public class Testing {
 		List<ComplDef> listOfComplDef = new ArrayList<>();
 		ComplDef ComplDef = new ComplDef();
 		ComplDef.setNewSecJql("test1ibhbbtesti12");
-		ComplDef.setSecJql("testing");
+		ComplDef.setSecJql("test1_udf$fxvdbbrrnr*fvbdbb");
 		listOfComplDef.add(ComplDef);
-		complDefMap.put("test1_udf", listOfComplDef);
+		complDefMap.put("test1_", listOfComplDef);
 
 		// replacement to be done
+
+		// Key is the pcolDef tags
+		// value is basically the list containing whole of the jql formed
 
 		for (Map.Entry<String, List<ComplDef>> mm : complDefMap.entrySet()) {
 
 			for (ComplDef complDef : mm.getValue()) {
-				String response = containsSecJql(mm.getValue(), complDef.getSecJql());
+				String secJql = containsSecJql(mm.getValue(), mm.getKey());
+				if (updateColTagFlag) {
+					System.out.println("Updated Col Tag");
 
+					// you need to store the updated coltag and the values and format the final
+					// List....So we can combine whole of the
+					// data now
+					// we have the BQL Scan Response and then we can formaulate the BQL Replace
+					// Response for the final
+					// BQL Consist of the Col_Tag as the Key +program+BQL Function ..If the Col_tag
+					// is comma Separated could be printed
+					// after retrieving the value from the Key easily then ...Otherwise would'nt be
+					// possible though
+
+				} else {
+
+					System.out.println("Remians the same ");
+				}
 				// Do the manupulation of response
 
 			}
@@ -54,10 +75,11 @@ public class Testing {
 	}
 
 	public static String containsSecJql(final List<ComplDef> list, final String secJql) {
-
-		if (list.stream().anyMatch(ti -> ti.getSecJql() == secJql)) {
+		updateColTagFlag = false;
+		if (list.stream().anyMatch(ti -> ti.getSecJql().contains(secJql))) {
 			// contains the id
-			return list.stream().filter(ti -> ti.getSecJql() == secJql).findFirst().get().getNewSecJql();
+			updateColTagFlag = true;
+			return list.stream().filter(ti -> ti.getSecJql().contains(secJql)).findFirst().get().getNewSecJql();
 		}
 		return secJql;
 	}
